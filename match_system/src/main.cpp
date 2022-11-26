3104077c092c8bde4fa11ebe733333f37b129dd7
Kay-Rick <Kay_Rick@outlook.com>
2022-11-26 21:12:53 +0800

    score match

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -15,6 +15,7 @@
 #include <condition_variable>
 #include <queue>
 #include <vector>
+#include <unistd.h>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -62,10 +63,27 @@ class Pool {
 
         void match() {
             while (users.size() > 1) {
-                auto a = users[0], b = users[1];
-                users.erase(users.begin());
-                users.erase(users.begin());
-                save_result(a.id, b.id);
+                // auto a = users[0], b = users[1];
+                // users.erase(users.begin());
+                // users.erase(users.begin());
+                // save_result(a.id, b.id);
+
+                sort(users.begin(), users.end(), [&](User& a, User& b) {
+                    return a.score < b.score;
+                });
+                
+                bool flag = true;
+                for (uint32_t i = 1; i < users.size(); i++) {
+                    auto a = users[i], b = users[i - 1];
+                    if (a.score - b.score <= 100) {
+                        users.erase(users.begin() + i - 1, users.begin() + i + 1);                        
+                        save_result(a.id, b.id);
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                    break;
             }
         }
 
@@ -117,7 +135,10 @@ void consume_task() {
         unique_lock<mutex> lck(message_queue.m);
         if (message_queue.q.empty()) {
             // cv.wait(lck) will operate lck.unlock()
-            message_queue.cv.wait(lck);
+            // message_queue.cv.wait(lck);
+            lck.unlock();
+            pool.match();
+            sleep(1);
             continue;
         }
         else {
