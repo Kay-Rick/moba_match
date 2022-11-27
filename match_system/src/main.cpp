f77a67351867ac22fb69afd5dd5c1268548aeb73
Kay-Rick <Kay_Rick@outlook.com>
2022-11-27 19:41:39 +0800

    add multithreading for server

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -3,12 +3,16 @@
 
 #include "match_server/Match.h"
 #include "save_client/Save.h"
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/concurrency/ThreadFactory.h>
+#include <thrift/server/TThreadedServer.h>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransportUtils.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <thrift/TToString.h>
 
 #include <iostream>
 #include <thread>
@@ -28,183 +32,202 @@ using namespace ::match_service;
 using namespace ::save_service;
 
 struct Task {
-    User user;
-    string type;
+	User user;
+	string type;
 };
 
 struct MessageQueue {
-    queue<Task> q;
-    mutex m;
-    condition_variable cv;
+	queue<Task> q;
+	mutex m;
+	condition_variable cv;
 }message_queue;
 
 class Pool {
-    public:
+	public:
 
-        void save_result(int a, int b) {
-            cout << "Match Result: " << a << " " << b << endl;
-            std::shared_ptr<TTransport> socket(new TSocket("localhost", 9091));
-            std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-            std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-            SaveClient client(protocol);
+		void save_result(int a, int b) {
+			cout << "Match Result: " << a << " " << b << endl;
+			std::shared_ptr<TTransport> socket(new TSocket("localhost", 9091));
+			std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+			std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+			SaveClient client(protocol);
 
-            try {
-                transport->open();
+			try {
+				transport->open();
 
-                int res = client.save_data("rick", "xxxxxx", a, b);
+				int res = client.save_data("rick", "xxxxxx", a, b);
 
-                if (!res) puts("success");
-                else puts("failed");
+				if (!res) puts("success");
+				else puts("failed");
 
-                transport->close();
-            } catch (TException& tx) {
-                cout << "ERROR: " << tx.what() << endl;
-            }
-        }
+				transport->close();
+			} catch (TException& tx) {
+				cout << "ERROR: " << tx.what() << endl;
+			}
+		}
 
-        bool check_match(int i, int j) {
-            User a = users[i], b = users[j];
-            int dt = abs(a.score - b.score);
-            int a_max_dif = wts[i] * 50;
-            int b_max_dif = wts[j] * 50;
-            return dt <= a_max_dif && dt <= b_max_dif;
-        }
+		bool check_match(int i, int j) {
+			User a = users[i], b = users[j];
+			int dt = abs(a.score - b.score);
+			int a_max_dif = wts[i] * 50;
+			int b_max_dif = wts[j] * 50;
+			return dt <= a_max_dif && dt <= b_max_dif;
+		}
 
-        void match() {
-            for (uint32_t i = 0; i < wts.size(); i++) {
-                wts[i]++;
-            }
+		void match() {
+			for (uint32_t i = 0; i < wts.size(); i++) {
+				wts[i]++;
+			}
 
-            while (users.size() > 1) {
-                // auto a = users[0], b = users[1];
-                // users.erase(users.begin());
-                // users.erase(users.begin());
-                // save_result(a.id, b.id);
+			while (users.size() > 1) {
+				// auto a = users[0], b = users[1];
+				// users.erase(users.begin());
+				// users.erase(users.begin());
+				// save_result(a.id, b.id);
 
-                /*sort(users.begin(), users.end(), [&](User& a, User& b) {
-                    return a.score < b.score;
-                });
-                
-                bool flag = true;
-                for (uint32_t i = 1; i < users.size(); i++) {
-                    auto a = users[i], b = users[i - 1];
-                    if (a.score - b.score <= 100) {
-                        users.erase(users.begin() + i - 1, users.begin() + i + 1);                        
-                        save_result(a.id, b.id);
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag)
-                    break;*/
+				/*sort(users.begin(), users.end(), [&](User& a, User& b) {
+				  return a.score < b.score;
+				  });
 
-                bool flag = true;
-                for (uint32_t i = 0; i < users.size(); i++) {
-                    for (uint32_t j = i + 1; j < users.size(); j++) {
-                        if (check_match(i, j)) {
-                            auto a = users[i], b = users[j];
-                            users.erase(users.begin() + j);
-                            users.erase(users.begin() + i);
-                            wts.erase(wts.begin() + j);
-                            wts.erase(wts.begin() + i);
-                            save_result(a.id, b.id);
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (!flag)
-                        break;
-                }
-                
-                if (flag)
-                    break;
+				  bool flag = true;
+				  for (uint32_t i = 1; i < users.size(); i++) {
+				  auto a = users[i], b = users[i - 1];
+				  if (a.score - b.score <= 100) {
+				  users.erase(users.begin() + i - 1, users.begin() + i + 1);                        
+				  save_result(a.id, b.id);
+				  flag = false;
+				  break;
+				  }
+				  }
+				  if (flag)
+				  break;*/
 
-            }
-        }
+				bool flag = true;
+				for (uint32_t i = 0; i < users.size(); i++) {
+					for (uint32_t j = i + 1; j < users.size(); j++) {
+						if (check_match(i, j)) {
+							auto a = users[i], b = users[j];
+							users.erase(users.begin() + j);
+							users.erase(users.begin() + i);
+							wts.erase(wts.begin() + j);
+							wts.erase(wts.begin() + i);
+							save_result(a.id, b.id);
+							flag = false;
+							break;
+						}
+					}
+					if (!flag)
+						break;
+				}
 
-        void add (User user) {
-            users.push_back(user);
-            wts.push_back(0);
-        }
+				if (flag)
+					break;
 
-        void remove(User user) {
-            for (uint32_t i = 0; i < users.size(); i++) {
-                if (users[i].id == user.id) {
-                    users.erase(users.begin() + i);
-                    wts.erase(wts.begin() + i);
-                    break;
-                }
-            }
-        }
+			}
+		}
 
-    private:
-        vector<User> users;
-        vector<int> wts;
+		void add (User user) {
+			users.push_back(user);
+			wts.push_back(0);
+		}
+
+		void remove(User user) {
+			for (uint32_t i = 0; i < users.size(); i++) {
+				if (users[i].id == user.id) {
+					users.erase(users.begin() + i);
+					wts.erase(wts.begin() + i);
+					break;
+				}
+			}
+		}
+
+	private:
+		vector<User> users;
+		vector<int> wts;
 }pool;
 
 class MatchHandler : virtual public MatchIf {
-    public:
-        MatchHandler() {
-            // Your initialization goes here
-        }
+	public:
+		MatchHandler() {
+			// Your initialization goes here
+		}
 
-        int32_t add_user(const User& user, const std::string& info) {
-            // Your implementation goes here
-            printf("add_user\n");
-            unique_lock<mutex> lck(message_queue.m);
-            message_queue.q.push({user, "add"});
-            message_queue.cv.notify_all();
-            return 0;
-        }
+		int32_t add_user(const User& user, const std::string& info) {
+			// Your implementation goes here
+			printf("add_user\n");
+			unique_lock<mutex> lck(message_queue.m);
+			message_queue.q.push({user, "add"});
+			message_queue.cv.notify_all();
+			return 0;
+		}
 
-        int32_t remove_user(const User& user, const std::string& info) {
-            // Your implementation goes here
-            printf("remove_user\n");
-            unique_lock<mutex> lck(message_queue.m);
-            message_queue.q.push({user, "remove"});
-            message_queue.cv.notify_all();
-            return 0;
-        }
+		int32_t remove_user(const User& user, const std::string& info) {
+			// Your implementation goes here
+			printf("remove_user\n");
+			unique_lock<mutex> lck(message_queue.m);
+			message_queue.q.push({user, "remove"});
+			message_queue.cv.notify_all();
+			return 0;
+		}
+
+};
+
+class MatchCloneFactory : virtual public MatchIfFactory {
+	public:
+		~MatchCloneFactory() override = default;
+		MatchIf* getHandler(const ::apache::thrift::TConnectionInfo& connInfo) override
+		{
+			std::shared_ptr<TSocket> sock = std::dynamic_pointer_cast<TSocket>(connInfo.transport);
+			/*cout << "Incoming connection\n";
+			  cout << "\tSocketInfo: "  << sock->getSocketInfo() << "\n";
+			  cout << "\tPeerHost: "    << sock->getPeerHost() << "\n";
+			  cout << "\tPeerAddress: " << sock->getPeerAddress() << "\n";
+			  cout << "\tPeerPort: "    << sock->getPeerPort() << "\n";*/ 
+			return new MatchHandler;
+
+		}
+		void releaseHandler(MatchIf* handler) override {
+			delete handler;
+
+		}
 
 };
 
 void consume_task() {
-    while (true) {
-        unique_lock<mutex> lck(message_queue.m);
-        if (message_queue.q.empty()) {
-            // cv.wait(lck) will operate lck.unlock()
-            // message_queue.cv.wait(lck);
-            lck.unlock();
-            pool.match();
-            sleep(1);
-        }
-        else {
-            auto task = message_queue.q.front();
-            message_queue.q.pop();
-            // do task
-            lck.unlock();
+	while (true) {
+		unique_lock<mutex> lck(message_queue.m);
+		if (message_queue.q.empty()) {
+			// cv.wait(lck) will operate lck.unlock()
+			// message_queue.cv.wait(lck);
+			lck.unlock();
+			pool.match();
+			sleep(1);
+		}
+		else {
+			auto task = message_queue.q.front();
+			message_queue.q.pop();
+			// do task
+			lck.unlock();
 
-            if (task.type == "add") {
-                pool.add(task.user);
-            } else {
-                pool.remove(task.user);
-            }
-        }
-    }
+			if (task.type == "add") {
+				pool.add(task.user);
+			} else {
+				pool.remove(task.user);
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv) {
-    int port = 9090;
-    ::std::shared_ptr<MatchHandler> handler(new MatchHandler());
-    ::std::shared_ptr<TProcessor> processor(new MatchProcessor(handler));
-    ::std::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-    ::std::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-    ::std::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
-
-    TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-    cout << "start server" << endl;
-    thread matching_thread(consume_task);
-    server.serve();
-    return 0;
+	TThreadedServer server(
+		std::make_shared<MatchProcessorFactory>(std::make_shared<MatchCloneFactory>()),
+		std::make_shared<TServerSocket>(9090), //port
+		std::make_shared<TBufferedTransportFactory>(),
+		std::make_shared<TBinaryProtocolFactory>());
+	
+	cout << "start server" << endl;
+	thread matching_thread(consume_task);
+	server.serve();
+	return 0;
 }
 
